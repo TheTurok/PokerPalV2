e0ebe306c68aa17e34ce0f292f80b6af466bcb2f
Timothy Choi <tchoi@ucsd.edu>
2016-10-13 19:20:56 -0700

    Finished Main Layout - It was a Bitch

---
+++ app/src/main/java/tjcrew/pokerpalv2/BackEnd/Players.java
@@ -0,0 +1,26 @@
+package tjcrew.pokerpalv2.BackEnd;
+
+/**
+ * Created by TheTurok on 10/13/2016.
+ */
+public class Players {
+
+    //You will implement the class of players
+    //make sure it has all the information
+    // also if you can possible keep the array of all nine players in here, it would be great
+    //if there's a better place let me know
+
+
+
+    //I will also like you to make another class which handles all the buttons pressed by pokertable activity.
+    //To keep it simple, i want activity classes seperate from the actual class that deals with math and turns.
+
+
+    //Players - plyer Object
+    //Action - the things pleyrs can do when buttons are presssed
+    //Game - the turn base it goes through, and this would do all-in-logic, big blind small blind, etc.
+
+    //General would be Game -> choose next player info -> player can do an -> action
+
+    //Note player doesnt call action class, jwe jsut keep the functions in that class for simplicity
+}
