88cbd55dba8bfe4f46ba2bd9569182eadad448a1
Joey <ih8u2o@gmail.com>
2016-10-16 01:33:06 -0400

    Basic layout for Players. Started Actions

---
+++ app/src/main/java/tjcrew/pokerpalv2/BackEnd/Actions.java
@@ -0,0 +1,60 @@
+/*
+Actions.java
+
+'Actions' is a public static helper class designed to
+simplify the class declaration of Game and Players in 
+the app. There are no 'Actions' objects. Rather,
+'Actions' contains member functions that act
+on 'Game' and/or 'Players' objects in ways that correspond
+to actions taken by players during betting rounds of
+poker games.
+
+### Example usage
+
+public static void main(String[] args) {
+    //Game declared
+    Game newGame = new Game();
+    
+    //A player named "PlayerX" folds, so we ovewrwrite our old game with a new game
+    where PlayerX is folded.
+    newGame = Actions.fold(newGame, "PlayerX");
+    //A player named "PlayerY" checks
+    newGame = Actions.check(newGame, "PlayerY");
+
+} 
+
+*/
+
+import Players.java
+
+public static class Actions {
+    
+
+    /*
+        fold is a function that affects both Game and Players objects. 
+        It will change the status of aPlayer in aGame to FOLDED 
+        provided that aPlayer is in aGame. If aPlayer is not in a game,
+        we will throw an exception.
+
+        @param aGame: A valid Game object with players. fold should not
+        be called if there are no players in the game, so fold throws
+        an exception in that case. 
+
+        @param aPlayer: A valid Players object who wishes to perform a
+        fold action during their turn on the current betting round. 
+        aPlayer should be a Players within the Game object, and fold
+        throws an exception if this case is not met. 
+
+        @return aGame: Provided that aGame and aPlayer meet all the
+        required conditions, fold returns aGame with aPlayer that has
+        the status FOLDED.
+    */
+    public Game fold(Game aGame, Players aPlayer) {
+        //Check aGame to make sure that aPlayer is actually in aGame
+
+        //If the player is in the game, set their status to folded
+
+        return aGame;
+
+    }
+}
--- app/src/main/java/tjcrew/pokerpalv2/BackEnd/Players.java
+++ app/src/main/java/tjcrew/pokerpalv2/BackEnd/Players.java
@@ -9,10 +9,92 @@ import tjcrew.pokerpalv2.FrontEnd.PokerTable;
 /**
  * Created by TheTurok on 10/13/2016.
  */
+
+/*
+    Players
+
+    'Players' is a class designed to hold player information during the course of a 
+    game session. A 'Players' object contains a static reference to all players in 
+    the form of an array. It also contains information about each player, including
+    their name, how much money they have, how much they last bet, and their current
+    state in the game.
+
+    The master list of players and the index of the last player who made a raise in that
+    array are in public scope.
+
+    ### Example usage
+
+    public static void main(String[] args) {
+        //make a new Player
+        Players firstPlayer = new Players(5,"FirstPlayer");
+    }
+
+    Implementation started October 14, 2016 by
+    jalbatross 
+*/
 public class Players {
+    public static enum Status {
+        ACTIONABLE, ALLIN, FOLD, SITOUT; 
+    }
 
+    public static ArrayList<Players> masterList;
+    public static int indexOfLastRaiser;
 
+    private boolean masterListInitialized;
+    private double money;
+    private String name;
 
+    /*
+    Players default constructor
+    @param: none
+
+    @return: A Players object with money 0, lastBet 0, and name noName.
+
+    */
+    public Players() {
+        if (!masterListInitialized)
+        {
+            masterList = new ArrayList<Players>();
+            masterListInitialzed = true;
+        }
+
+        money = 0;
+        name = "noName";
+    }
+
+    /*
+    Players constructor with all parameters. Used to construct a Players object
+    with a specified name and amount of starting money. If the Players object
+    to be created is vaid it is pushed to the back of the masterList of Players.
+
+    @param money_: A positive double value used to initialize the starting chip
+    amount of the Players object. A negative value throws an exception.
+
+    @param name_: String used to initialize the displayed name of the Players
+    object. 
+
+    @return: A Players object initialized with money_ worth of chips and a 
+    displayed name of name_.
+    */
+    public Players(double money_, String name_) {
+        if (!masterListInitialized)
+        {
+            masterList = new ArrayList<Players>();
+            masterListInitialized = true;
+        }
+
+        if (money_ <= 0)
+            throw new IndexOutOfBoundsException("Must start with valid money value.");
+        
+        //Check for valid names
+        //Fix name or throw exception if name is invalid
+
+        name = name_;
+
+        masterList.push_back(this);
+    }
+
+    
     //You will implement the class of players, and more
     //make sure it has all the information
     // also if you can possible keep the array of all nine players in here, it would be great
